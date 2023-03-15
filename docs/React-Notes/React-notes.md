cf63fc7722cfcce520e267990d9135d74ad103f6
Ercan Er <ercan@thundra.io>
2023-03-15 15:42:14 +0300

    doc version 1

---
+++ docs/React-Notes/React-notes.md
@@ -0,0 +1,8 @@
+---
+sidebar_label: "React - useState"
+sidebar_position: 1
+---
+
+# useState()
+
+This is my **first Docusaurus document**!
---
+++ src/pages/my-react-page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MyReactPage = () => {
+     return (
+          <div>
+               <h1>Test</h1>
+               Ercan Er
+          </div>
+     );
+};
+
+export default MyReactPage;
