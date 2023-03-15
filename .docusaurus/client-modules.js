9251fc91c28d63acea7c9267c797fd0c2bc7e851
Ercan Er <ercan@thundra.io>
2023-03-15 16:53:06 +0300

    docusaurus file

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/ercaner/Desktop/ercan-er/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/ercaner/Desktop/ercan-er/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/ercaner/Desktop/ercan-er/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/ercaner/Desktop/ercan-er/src/css/custom.css'),
+];
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,11 +11,11 @@ const config = {
      favicon: "img/favicon.ico",
 
      // Set the production url of your site here
-     url: "https://your-docusaurus-test-site.com",
+     url: "https://ercan-er.github.io",
      // Set the /<baseUrl>/ pathname under which your site is served
      // For GitHub pages deployment, it is often '/<projectName>/'
      baseUrl: "/",
-
+     deploymentBranch: "main",
      // GitHub pages deployment config.
      // If you aren't using GitHub pages, you don't need these.
      organizationName: "ercan-er", // Usually your GitHub org/user name.
